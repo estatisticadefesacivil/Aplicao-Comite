441951b96b1879c16923bb1422996e236a17936d
estatisticadefesacivil <estatistica.analisededados@defesacivil.am.gov.br>
2025-04-21 12:06:34 -0400

    teste One Drive

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return redirect(url_for('login'))
-app.config['SECRET_KEY'] = 'chave-secreta'  # Alterar para uma chave segura
-#app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#app.config['SECRET_KEY'] = 'chave-secreta'  # Alterar para uma chave segura
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
